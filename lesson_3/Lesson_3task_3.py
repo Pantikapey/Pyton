d76bee9e0fbf21f02ded609e3c107f20a8a3b06b
Pantikapey <19antoxa77@gmail.com>
2026-02-26 20:56:04 +0300

    Update Lesson_3task_3.py

---
+++ lesson_3/Lesson_3task_3.py
@@ -0,0 +1,11 @@
+from address import Address
+from lesson_3_mailing import Mailing
+
+to_address = Address
+from_address = Address
+cost = 1500
+track = "123654"
+
+mailing = Mailing(to_address, from_address, 1500, "123654")
+
+print(mailing)
