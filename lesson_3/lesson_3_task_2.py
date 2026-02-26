4d18de19d2772d7e8261b30b3c261f7291fd8a08
Pantikapey <19antoxa77@gmail.com>
2026-02-26 11:46:48 +0300

    Update lesson_3_task_2.py

--- lesson_3/lesson_3_task_2.py
+++ lesson_3/lesson_3_task_2.py
@@ -2,11 +2,11 @@ from smartphone import Smartphone
 
 # Создаем список телефонов (Catalog)
 Catalog = [
-    Smartphone("Nokia", 3120, number +7985),
-    Smartphone("Nokia", 3133, number +8214),
-    Smartphone("Nokia", 3154, number +6544),
-    Smartphone("Nokia", 3166, number +1245),
-    Smartphone("Nokia", 3220, number +2541)
+    Smartphone("Nokia", 3120, +7985),
+    Smartphone("Nokia", 3133, +8214),
+    Smartphone("Nokia", 3154, +6544),
+    Smartphone("Nokia", 3166, +1245),
+    Smartphone("Nokia", 3220, +2541)
 ]
 
 # Печатаем каталог
