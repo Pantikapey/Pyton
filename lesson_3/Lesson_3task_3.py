cb5ad1c33718609bf3fcdb37e238c401d773ab24
Pantikapey <19antoxa77@gmail.com>
2026-02-27 19:11:17 +0300

    Update Lesson_3task_3.py

--- lesson_3/Lesson_3task_3.py
+++ lesson_3/Lesson_3task_3.py
@@ -1,11 +1,9 @@
-from address import Address
+from lesson_3_address import Address
 from lesson_3_mailing import Mailing
 
-to_address = Address
-from_address = Address
-cost = 1500
-track = "123654"
+to_address = Address("Москва", "Тверская", 1, 10, "125009")
+from_address = Address("Курск", "Ленина", 5, 12, "305000")
 
 mailing = Mailing(to_address, from_address, 1500, "123654")
-
 print(mailing)
+
