694d51788b9368de9b0eddcda801bab9d97ca48d
Pantikapey <19antoxa77@gmail.com>
2026-03-24 07:52:07 +0300

    ddddd

--- lesson_4/test_string_utils.py
+++ lesson_4/test_string_utils.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 import pytest
 from string_utils import StringUtils
 
@@ -11,7 +13,7 @@ string_utils = StringUtils()
     ("hello world", "Hello world"),
     ("python", "Python"),
 ])
-def test_capitalize_positive(input_str, expected):
+def test_capitalize_positive(input_str: Literal['skypro'] | Literal['hello world'] | Literal['python'], expected: Literal['Skypro'] | Literal['Hello world'] | Literal['Python']):
     assert string_utils.capitalize(input_str) == expected
 
 
@@ -21,6 +23,61 @@ def test_capitalize_positive(input_str, expected):
     ("", ""),
     ("   ", "   "),
 ])
-def test_capitalize_negative(input_str, expected):
+def test_capitalize_negative(input_str: Literal['123abc'] | Literal[''] | Literal['   '], expected: Literal['123abc'] | Literal[''] | Literal['   ']):
     assert string_utils.capitalize(input_str) == expected
 
+
+@pytest.mark.positive
+@pytest.mark.parametrize("input_str, expected", [
+    ("  skypro", "Skypro"),
+    ("  Hello world", "Hello world"),
+    ("  Python", "Python"),
+])
+def test_trim_positive(input_str: Literal['  skypro'] | Literal['  Hello world'] | Literal['  Python'], expected: Literal['Skypro'] | Literal['Hello world'] | Literal['Python']):
+    assert string_utils.trim(input_str) == expected
+
+
+@pytest.mark.negative
+@pytest.mark.parametrize("input_str, expected", [
+    ("  123abc", "123abc"),
+    ("  ", ""),
+    ("   ", ""),
+])
+def test_trim_negative(input_str: Literal['  123abc'] | Literal['  '] | Literal['   '], expected: Literal['123abc'] | Literal['']):
+    assert string_utils.trim(input_str) == expected
+
+
+@pytest.mark.positive
+@pytest.mark.parametrize("input_str, expected", [
+    ("SkyPro", "'S' -> True"),
+    ("SkyPro", "'U' -> False")
+])
+def test_contains_positive(input_str: Literal['SkyPro'], expected: Literal['\'S\' -> True'] | Literal['\'U\' -> False']):
+    assert string_utils.contains(input_str) == expected
+
+
+@pytest.mark.negative
+@pytest.mark.parametrize("input_str, expected", [
+    ("123abc", "'1' -> True"),
+    
+])
+def test_contains_negative(input_str: Literal['123abc'], expected: Literal['\'1\' -> True']):
+    assert string_utils.contains(input_str) == expected
+
+
+@pytest.mark.positive
+@pytest.mark.parametrize("input_str, expected", [
+     ("SkyPro", "'k' -> SyPro"),
+     ("SkyPro", "'Pro' -> Sky")
+])
+def test_delete_symbol_positive(input_str: Any, expected: Any):
+    assert string_utils.delete_symbol(input_str) == expected
+
+
+@pytest.mark.negative
+@pytest.mark.parametrize("input_str, expected", [
+    ("SkyPro", "'k' -> SyPro"),
+    ("SkyPro", "'Pro' -> Sky")
+])
+def test_delete_symbol_negative(input_str: Literal['123abc'], expected: Literal['\'1\' -> True']):
+    assert string_utils.delete_symbol(input_str) == expected
