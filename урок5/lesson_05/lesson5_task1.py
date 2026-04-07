499e6311d1ef0c4c0478903e383010f51e937812
Pantikapey <19antoxa77@gmail.com>
2026-04-07 21:39:01 +0300

    Update lesson5_task1.py

--- урок5/lesson_05/lesson5_task1.py
+++ урок5/lesson_05/lesson5_task1.py
@@ -1,1 +1,15 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def test_class_name():
+    driver = webdriver.Chrome()
+    driver.get("https://www.selenium.dev/selenium/web/locators_tests/locators.html")
+    element = driver.find_element(By.CLASS_NAME, "information")
+
+    assert element is not None
+    assert element.tag_name == "input"
+
+    driver.quit()
 
