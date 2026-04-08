8c3a671d68659845a1bcef6725125c457edaf7b8
19Antoxa77 <Victoriy@ptct.net>
2026-04-08 08:50:24 +0300

    ff

--- урок5/lesson_05/lesson5_task1.py
+++ урок5/lesson_05/lesson5_task1.py
@@ -1,11 +1,9 @@
-import pytest
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
-def test_class_name():
-    driver = webdriver.Chrome()
-    driver.get("https://www.selenium.dev/selenium/web/locators_tests/locators.html")
+driver = webdriver.Chrome()
+driver.get("https://www.selenium.dev/selenium/web/locators_tests/locators.html")
     element = driver.find_element(By.CLASS_NAME, "information")
 
     assert element is not None
