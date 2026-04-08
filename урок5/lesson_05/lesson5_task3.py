6ffde33708289c48791c0625df2447fdaa47d2ca
Pantikapey <19antoxa77@gmail.com>
2026-04-08 04:17:56 +0300

    Update lesson5_task3.py

--- урок5/lesson_05/lesson5_task3.py
+++ урок5/lesson_05/lesson5_task3.py
@@ -1,1 +1,9 @@
+  from selenium.webdriver.firefox.options import Options
+    from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 
+    options = Options()
+    firefox_profile = FirefoxProfile()
+    firefox_profile.set_preference("javascript.enabled", False)
+    options.profile = firefox_profile
+
+    driver = webdriver.Firefox(options=options)
