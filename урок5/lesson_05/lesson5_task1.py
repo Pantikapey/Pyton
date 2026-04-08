831b74f86d944e46c01b1e568dc74ed8c60c40fb
19Antoxa77 <Victoriy@ptct.net>
2026-04-08 19:25:23 +0300

    dfd

--- урок5/lesson_05/lesson5_task1.py
+++ урок5/lesson_05/lesson5_task1.py
@@ -1,14 +1,21 @@
-import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+driver = webdriver.Chrome()
 
-def test_class_name():
-    driver = webdriver.Chrome()
-    driver.get("http://uitestingplayground.com/classattr")
-    element = driver.find_element(By.CLASS_NAME, "btn class1 btn-privary btn-test")
+driver.get("https://www.selenium.dev/selenium/web/web-form.html")
 
-    assert element is not None
-    assert element.tag_name == "input"
+title = driver.title
 
-    driver.quit()
+driver.implicitly_wait(0.5)
+
+text_box = driver.find_element(by=By.NAME, value="my-text")
+submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+
+text_box.send_keys("Selenium")
+submit_button.click()
+
+message = driver.find_element(by=By.ID, value="message")
+text = message.text
+
+driver.quit()
