216c50c0a25d8c3c63d97fba8c29b04fe5d527c3
19Antoxa77 <Victoriy@ptct.net>
2026-04-08 04:35:10 +0300

    gffg

--- урок5/lesson_05/lesson5_task4.py
+++ урок5/lesson_05/lesson5_task4.py
@@ -1,1 +1,14 @@
+import os
+import subprocess
+import sys
+
+import pytest
+from selenium import webdriver
+
+
+def test_basic_options():
+    options = webdriver.FirefoxOptions()
+    driver = webdriver.Firefox(options=options)
+
+    driver.quit()
 
