4195f8eb033291d58c9c16c5250476cc16bbc478
Pantikapey <19antoxa77@gmail.com>
2026-04-27 06:46:20 +0300

    Update lesson5_task1.py

--- урок5/lesson_05/lesson5_task1.py
+++ урок5/lesson_05/lesson5_task1.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
-
 driver.get("http://uitestingplayground.com/classattr.")
 sleep(10)
 driver.find_element(By.CSS_SELECTOR, "body > section > div > button.btn.class2.btn-primary.btn-test").click()
