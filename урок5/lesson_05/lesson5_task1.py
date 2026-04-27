5ec2e44b6d4ff1455a338e9e09e4c47318a20f0b
Pantikapey <19antoxa77@gmail.com>
2026-04-27 06:38:49 +0300

    Update lesson5_task1.py

--- урок5/lesson_05/lesson5_task1.py
+++ урок5/lesson_05/lesson5_task1.py
@@ -1,21 +1,11 @@
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
-driver.get("https://www.selenium.dev/selenium/web/web-form.html")
-
-title = driver.title
-
-driver.implicitly_wait(0.5)
-
-text_box = driver.find_element(by=By.NAME, value="my-text")
-submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
-
-text_box.send_keys("Selenium")
-submit_button.click()
-
-message = driver.find_element(by=By.ID, value="message")
-text = message.text
+driver.get("http://uitestingplayground.com/classattr.")
+sleep(10)
+driver.find_element(By.CSS_SELECTOR, "body > section > div > button.btn.class2.btn-primary.btn-test").click()
 
 driver.quit()
