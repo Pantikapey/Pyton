1d6cbb13d48d2b5fc7adc35e4b7125bd0bd3b077
Pantikapey <19antoxa77@gmail.com>
2026-04-27 08:15:16 +0300

    Update lesson5_task3.py

--- урок5/lesson_05/lesson5_task3.py
+++ урок5/lesson_05/lesson5_task3.py
@@ -1,10 +1,13 @@
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Firefox()
 driver.get("http://the-internet.herokuapp.com/inputs")
-
-
+sleep(10)
+driver.find_element(By.CSS_SELECTOR,".example > input:nth-child(2)").clear()
+sleep(5)
 driver.quit()                 
+
 
     
