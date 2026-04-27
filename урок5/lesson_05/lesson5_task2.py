931c3fb836a70105757667ac78556df26818b1f5
Pantikapey <19antoxa77@gmail.com>
2026-04-27 06:50:27 +0300

    Update lesson5_task2.py

--- урок5/lesson_05/lesson5_task2.py
+++ урок5/lesson_05/lesson5_task2.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.get(" http://uitestingplayground.com/dynamicid.")
-
 driver.find_element(By.LINK_TEXT, "Button with Dynamic ID").click()
 
 driver.quit()
